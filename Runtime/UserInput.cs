6f85363280899333ebaba65f897a4c7b24426512
yimnabin <yimnabin20051022@gmail.com>
2025-01-14 17:06:44 +0900

    FIRST PACKAGE UPLOAD

---
+++ Runtime/UserInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace CustomUtility
+{
+	public class UserInput : SingleTon<UserInput>
+	{
+
+	} 
+}
